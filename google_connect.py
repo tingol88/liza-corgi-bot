f9c637722c52bf13a35d3ffc51437dbbf5abd1b3
Alexander Blinkov <tingol88@yandex.ru>
2025-04-15 09:03:13 +0300

    Update google_connect.py

--- google_connect.py
+++ google_connect.py
@@ -1,6 +1,7 @@
 import os
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from googleapiclient import errors
 
 GOOGLE_CREDENTIALS_PATH = os.environ["GOOGLE_CREDENTIALS_PATH"]
 
